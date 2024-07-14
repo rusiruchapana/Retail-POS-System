fc90d3c53e55c04307bf33813f1ad6d05296b4aa
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-14 21:05:18 +0530

    Update CustomerEntity.java

--- pos/src/main/java/com/rusiruchapana/pos/entity/CustomerEntity.java
+++ pos/src/main/java/com/rusiruchapana/pos/entity/CustomerEntity.java
@@ -1,5 +1,10 @@
 package com.rusiruchapana.pos.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
 public class CustomerEntity {
-    
+
 }
