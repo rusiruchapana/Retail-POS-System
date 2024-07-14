4ce7a66fdf7cbc368fa4af01942080db1774196f
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-14 20:59:26 +0530

    Update CustomerController.java

--- pos/src/main/java/com/rusiruchapana/pos/controller/CustomerController.java
+++ pos/src/main/java/com/rusiruchapana/pos/controller/CustomerController.java
@@ -1,4 +1,15 @@
 package com.rusiruchapana.pos.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
+
+    @GetMapping("/text-1")
+    public String getText(){
+        return "Rusiru chapana";
+    }
 }
