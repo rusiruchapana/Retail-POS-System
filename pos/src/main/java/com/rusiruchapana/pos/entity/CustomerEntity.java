f3f102b82be729d85a67c866ca0728bfc6f4487d
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-14 21:36:56 +0530

    Update CustomerEntity.java

--- pos/src/main/java/com/rusiruchapana/pos/entity/CustomerEntity.java
+++ pos/src/main/java/com/rusiruchapana/pos/entity/CustomerEntity.java
@@ -1,7 +1,7 @@
 package com.rusiruchapana.pos.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
 
 import java.util.ArrayList;
 
@@ -9,12 +9,21 @@ import java.util.ArrayList;
 @Table(name = "customer")
 public class CustomerEntity {
 
+    @Id
+    @Column(name = "customer_id",length = 45)
     private Long customerId;
+    @Column(name = "customer_name",nullable = false,length = 100)
     private String customerName;
+    @Column(name = "customer_adress",length = 100)
     private String customerAdress;
-    private Double salary;
+    @Column(name = "customer_salary",length = 10)
+    private Double customerSalary;
+    @Type(type="json")
+    @Column(name = "contact_numbers",columnDefinition = "json")
     private ArrayList contactNumbers;
-    private String NicNumber;
+    @Column(name = "nic_number",nullable = false,unique = true)
+    private String nicNumber;
+    @Column(name = "active_status")
     private Boolean activeStatus;
 
 }
