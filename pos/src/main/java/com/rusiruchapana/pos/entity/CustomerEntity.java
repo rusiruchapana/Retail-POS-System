1217f610e5092305a6ec53d6d7c60c25e1ac9c70
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-21 10:46:22 +0530

    No/All args constructors.getters and setters create.

--- pos/src/main/java/com/rusiruchapana/pos/entity/CustomerEntity.java
+++ pos/src/main/java/com/rusiruchapana/pos/entity/CustomerEntity.java
@@ -1,14 +1,19 @@
 package com.rusiruchapana.pos.entity;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import jakarta.persistence.*;
-import lombok.Data;
-import org.hibernate.annotations.Columns;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
 import java.util.ArrayList;
 
 
 @Entity
 @Table(name = "customer")
+//@TypeDefs(
+//        @TypeDef(name="json", typeClass= JsonType.class)
+//)
 public class CustomerEntity {
 
     @Id
@@ -20,12 +25,81 @@ public class CustomerEntity {
     private String customerAdress;
     @Column(name = "customer_salary",length = 10)
     private Double customerSalary;
-    @Column(name = "nic_number",nullable = false,unique = true)
+    @Column(name = "nic_number",unique = true, length=12)
     private String nicNumber;
     @Convert(converter = ContactNumbersConverter.class)
     private ArrayList<String> contactNumbers;
-
     @Column(name = "active_status", columnDefinition = "TINYINT default 1")
     private Boolean activeStatus;
 
+    //No Args constructor.
+    public CustomerEntity() {
+    }
+
+    //All args constructor.
+    public CustomerEntity(Long customerId, String customerName, String customerAdress, Double customerSalary, String nicNumber, ArrayList<String> contactNumbers, Boolean activeStatus) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAdress = customerAdress;
+        this.customerSalary = customerSalary;
+        this.nicNumber = nicNumber;
+        this.contactNumbers = contactNumbers;
+        this.activeStatus = activeStatus;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAdress() {
+        return customerAdress;
+    }
+
+    public void setCustomerAdress(String customerAdress) {
+        this.customerAdress = customerAdress;
+    }
+
+    public Double getCustomerSalary() {
+        return customerSalary;
+    }
+
+    public void setCustomerSalary(Double customerSalary) {
+        this.customerSalary = customerSalary;
+    }
+
+    public String getNicNumber() {
+        return nicNumber;
+    }
+
+    public void setNicNumber(String nicNumber) {
+        this.nicNumber = nicNumber;
+    }
+
+    public ArrayList<String> getContactNumbers() {
+        return contactNumbers;
+    }
+
+    public void setContactNumbers(ArrayList<String> contactNumbers) {
+        this.contactNumbers = contactNumbers;
+    }
+
+    public Boolean getActiveStatus() {
+        return activeStatus;
+    }
+
+    public void setActiveStatus(Boolean activeStatus) {
+        this.activeStatus = activeStatus;
+    }
 }
