bf144ff5a4c770e704802c1b1fa6d0ae50f6c2a9
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-26 01:46:24 +0530

    Update CustomerMapper.java

--- pos/src/main/java/com/rusiruchapana/pos/util/CustomerMapper.java
+++ pos/src/main/java/com/rusiruchapana/pos/util/CustomerMapper.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
-    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+    //CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
 
     List<CustomerDTO> entity_to_dto(List<Customer> customers);
     CustomerDTO entity_to_dto(Customer customer);
