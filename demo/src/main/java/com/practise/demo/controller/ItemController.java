04300d4c5e8bc4150610210bcb8840e1f34df3e8
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-08-08 14:16:34 +0530

    Update ItemController.java

--- demo/src/main/java/com/practise/demo/controller/ItemController.java
+++ demo/src/main/java/com/practise/demo/controller/ItemController.java
@@ -1,14 +1,18 @@
 package com.practise.demo.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.practise.demo.dto.request.ItemResquestDTO;
+import com.practise.demo.util.StandardResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/item")
 public class ItemController {
 
-    
+    @PostMapping(path = "/create-item")
+    public ResponseEntity<StandardResponse> createItem(@RequestBody ItemResquestDTO itemResquestDTO){
+        return null;
+    }
 
 }
