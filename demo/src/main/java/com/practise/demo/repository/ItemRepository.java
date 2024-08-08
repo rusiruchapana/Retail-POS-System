c04615a0271902c26067f66b86494d3518e18aa3
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-08-08 14:21:46 +0530

    Update ItemRepository.java

--- demo/src/main/java/com/practise/demo/repository/ItemRepository.java
+++ demo/src/main/java/com/practise/demo/repository/ItemRepository.java
@@ -1,4 +1,7 @@
 package com.practise.demo.repository;
 
-public class ItemRepository {
+import com.practise.demo.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item,Long> {
 }
